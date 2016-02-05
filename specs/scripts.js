1617cfb1a3ce9794c3c3cad894c679d4ab185580
Wren Withers <wrenwithers@Wrens-MacBook-Air.local>
2016-02-05 10:23:16 -0800

    will pass updated second spec with altered parameters

--- specs/scripts.js
+++ specs/scripts.js
@@ -1,21 +1,21 @@
 
 
-function pizza(size, toppings) {
+function pizza(size, toppings){
   this.size = size;
   this.toppings = toppings;
   this.price= 10;
-
-
 }
 
-pizza.prototype.sizePrice = function() {
-  var price = this.price;
-  if (this.size === "small") {
-    this.price = this.price * 1;
-  } else if (this.size === "medium") {
-    this.price = this.price * 1.2;
-  } else {
-    price = 14;
-  }
-  return price;
- };
+ pizza.prototype.Price = function() {
+   var price = this.price;
+   var toppings = this.toppings;
+   if (this.size === "small") {
+     this.price = this.price * 1;
+   } else if (this.size === "medium") {
+     this.price = this.price * 1.2;
+   } else {
+     price = 14;
+   }
+   price = price + toppings;
+   return price;
+  };
--- specs/specs.js
+++ specs/specs.js
@@ -1,11 +1,15 @@
+
+
 describe('pizza', function() {
-  it("creates a new pizza with the input properties of size and topping number", function() {
-    var testPizza = new pizza("small",2);
-    expect(testPizza.size).to.equal("small");
-    expect(testPizza.toppings).to.eql(2);
-  });
-  it("creates a prototype for determining size price", function() {
-    var testPizza = new pizza("adsfsa", 2);
-    expect(testPizza.sizePrice()).to.equal(14);
-  });
-});
+   it("creates a new pizza with the input properties of size and topping number", function() {
+     var testPizza = new pizza("small",2);
+     expect(testPizza.size).to.equal("small");
+     expect(testPizza.toppings).to.equal(2);
+   });
+
+    it("creates a prototype for determining price", function() {
+     var testPizza = new pizza("large", 2);
+     expect(testPizza.Price()).to.equal(16);
+   });
+
+ })
